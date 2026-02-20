97c9df6f2d80cd364c625eb6cbf535942396b4c3
dmaters <198483623+dmaters@users.noreply.github.com>
2026-02-20 20:32:05 +0100

    Fix shadowmap glitches

--- src/scene/Light.cpp
+++ src/scene/Light.cpp
@@ -25,12 +25,10 @@ FrustumPoints getFrustumPoints(const Camera& camera) {
 	frustumVectors[3] = glm::vec3(-tanH, -tanV, -1.0f);
 
 	std::array<std::array<glm::vec4, 4>, 4> points;
-	glm::mat4 invView = glm::inverse(camera.getViewMatrix());
 	for (int pl = 0; pl < 4; pl++) {
 		for (int pi = 0; pi < 4; pi++) {
 			float distance = camera.getCascadeDistances()[pl];
-			points[pl][pi] =
-				invView * glm::vec4(frustumVectors[pi] * distance, 1.0);
+			points[pl][pi] = glm::vec4(frustumVectors[pi] * distance, 1.0);
 		}
 	}
 
@@ -43,14 +41,15 @@ Light::FrustumBounds Light::getFrustumBounds(
 	FrustumPoints fp = getFrustumPoints(camera);
 	std::array<glm::vec4, 8> lightSpaceBounds;
 
-	glm::mat4 view =
+	glm::mat4 cameraView = glm::inverse(camera.getViewMatrix());
+	glm::mat4 lightView =
 		glm::lookAt(orientation * position, glm::vec3(0), orientation[1]);
 
-	glm::mat4 cameraToView = view * glm::inverse(camera.getViewMatrix());
+	glm::mat4 cameraToLightView = lightView * cameraView;
 
 	for (int i = 0; i < 4; i++) {
-		lightSpaceBounds[i] = cameraToView * fp[cascade][i];
-		lightSpaceBounds[4 + i] = cameraToView * fp[cascade + 1][i];
+		lightSpaceBounds[i] = cameraToLightView * fp[0][i];
+		lightSpaceBounds[4 + i] = cameraToLightView * fp[cascade + 1][i];
 	}
 	float right = -1e9, left = 1e9, top = -1e9, bottom = 1e9, far = -1e9,
 		  near = 1e9;
@@ -64,22 +63,14 @@ Light::FrustumBounds Light::getFrustumBounds(
 		near = fmin(near, -lightSpaceBounds[i].z);
 	}
 
-	glm::vec4 worldCenter = view * glm::vec4(0, 0, 0, 1);
+	glm::vec4 worldCenterLightSpace = lightView * glm::vec4(0, 0, 0, 1);
 
-	right = fmin(right, worldCenter.x + sceneSize);
-	left = fmax(left, worldCenter.x - sceneSize);
-	top = fmin(top, worldCenter.y + sceneSize);
-	bottom = fmax(bottom, worldCenter.y - sceneSize);
-	far = fmax(far, -worldCenter.z + sceneSize);
-	near = fmin(near, -worldCenter.z - sceneSize);
-
-	float worldUnitsPerTexelX = (right - left) / SHADOWMAP_RES;
-	float worldUnitsPerTexelY = (top - bottom) / SHADOWMAP_RES;
-
-	left = floor(left / worldUnitsPerTexelX) * worldUnitsPerTexelX;
-	right = floor(right / worldUnitsPerTexelX) * worldUnitsPerTexelX;
-	bottom = floor(bottom / worldUnitsPerTexelY) * worldUnitsPerTexelY;
-	top = floor(top / worldUnitsPerTexelY) * worldUnitsPerTexelY;
+	right = fmin(right, worldCenterLightSpace.x + sceneSize);
+	left = fmax(left, worldCenterLightSpace.x - sceneSize);
+	top = fmin(top, worldCenterLightSpace.y + sceneSize);
+	bottom = fmax(bottom, worldCenterLightSpace.y - sceneSize);
+	far = fmax(far, -worldCenterLightSpace.z + sceneSize);
+	near = fmin(near, -worldCenterLightSpace.z - sceneSize);
 
 	float sizeX = right - left;
 	float sizeY = top - bottom;
@@ -114,7 +105,7 @@ Light::FrustumBounds Light::getFrustumBounds(
 		.left = left,
 		.top = top,
 		.bottom = bottom,
-		.padding = static_cast<float>((boundsVSize - maxVSize) * 0.5),
+		.padding = static_cast<float>((boundsHSize - maxHSize) * 0.5),
 	};
 }
 
@@ -125,7 +116,6 @@ MaterialDefinitions::Light Light::getShaderObject(
 	std::array<float, 3> shadowPaddings;
 	for (int i = 0; i < 3; i++) {
 		Light::FrustumBounds bounds = getFrustumBounds(camera, sceneSize, i);
-
 		glm::mat4 proj = glm::orthoRH_ZO(
 			bounds.left,
 			bounds.right,
