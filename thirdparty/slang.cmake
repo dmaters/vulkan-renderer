2b6c64dd947316843a83fe78acfdae3eb2b99962
dmaters <198483623+dmaters@users.noreply.github.com>
2026-07-22 11:59:00 +0200

    Fix runtime issues due to linking error

--- thirdparty/slang.cmake
+++ thirdparty/slang.cmake
@@ -1,9 +1,9 @@
 set(SLANG_VERSION "2025.23.2")
 
 if (WIN32)
-    set(SLANG_OS "windows-x86_64")
+  set(SLANG_OS "windows-x86_64")
 elseif (UNIX)
-    set(SLANG_OS "linux-x86_64")
+  set(SLANG_OS "linux-x86_64")
 endif ()
 
 include(FetchContent)
@@ -13,12 +13,12 @@ add_library(slang INTERFACE)
 
 target_include_directories(slang INTERFACE "${slang_zip_SOURCE_DIR}/include")
 if (WIN32)
-    target_link_libraries(slang INTERFACE "${slang_zip_SOURCE_DIR}/lib/slang-compiler.lib")
+  target_link_libraries(slang INTERFACE "${slang_zip_SOURCE_DIR}/lib/slang-compiler.lib")
 
-    set_target_properties(slang PROPERTIES
-            INTERFACE_DLL_PATH "${slang_zip_SOURCE_DIR}/bin/slang-compiler.dll;${slang_zip_SOURCE_DIR}/bin/slang-glslang.dll"
+  set_target_properties(slang PROPERTIES
+      INTERFACE_DLL_PATH
+      "${slang_zip_SOURCE_DIR}/bin/slang-compiler.dll;${slang_zip_SOURCE_DIR}/bin/slang-glslang.dll"
     )
 elseif (UNIX)
-    target_link_libraries(slang INTERFACE "${slang_zip_SOURCE_DIR}/lib/libslang-compiler.so.0.${SLANG_VERSION}"
-    )
+  target_link_libraries(slang INTERFACE "${slang_zip_SOURCE_DIR}/lib/libslang-compiler.so.0.${SLANG_VERSION}" "${slang_zip_SOURCE_DIR}/lib/libslang-glslang-${SLANG_VERSION}.so")
 endif ()
